a693a10c35f56e7d7f57b1ec2abc035dfa5b545f
pterror <pterrorbird@gmail.com>
2025-11-27 20:07:43 +1000

    feat[web]: add new script features to script editor

--- apps/web/src/components/ScriptEditor/types.ts
+++ apps/web/src/components/ScriptEditor/types.ts
@@ -4,7 +4,16 @@ export interface BlockDefinition {
   type: BlockType;
   label: string;
   opcode: string;
-  category: "logic" | "action" | "math" | "data";
+  category:
+    | "logic"
+    | "action"
+    | "math"
+    | "data"
+    | "time"
+    | "world"
+    | "list"
+    | "object"
+    | "func";
   layout?: "infix" | "standard" | "primitive" | "control-flow"; // New layout property
   slots?: {
     name: string;
@@ -307,6 +316,16 @@ export const BLOCK_DEFINITIONS: BlockDefinition[] = [
     ],
   },
   {
+    type: "statement",
+    label: "Give",
+    opcode: "give",
+    category: "action",
+    slots: [
+      { name: "Item", type: "block" },
+      { name: "Receiver", type: "block" },
+    ],
+  },
+  {
     type: "expression",
     label: "Create",
     opcode: "create",
@@ -314,10 +333,323 @@ export const BLOCK_DEFINITIONS: BlockDefinition[] = [
     slots: [{ name: "Data", type: "block" }],
   },
   {
+    type: "statement",
+    label: "Schedule",
+    opcode: "schedule",
+    category: "action",
+    slots: [
+      { name: "Verb", type: "string" },
+      { name: "Args", type: "block" },
+      { name: "Delay", type: "number" },
+    ],
+  },
+  {
+    type: "statement",
+    label: "Broadcast",
+    opcode: "broadcast",
+    category: "action",
+    slots: [
+      { name: "Message", type: "block" },
+      { name: "Location", type: "block", default: null },
+    ],
+  },
+  {
     type: "expression",
     label: "Random",
     opcode: "random",
     category: "math",
     slots: [],
+  },
+  {
+    type: "expression",
+    label: "Floor",
+    opcode: "floor",
+    category: "math",
+    slots: [{ name: "Val", type: "block" }],
+  },
+
+  // Time
+  {
+    type: "expression",
+    label: "Now",
+    opcode: "time.now",
+    category: "time",
+    slots: [],
+  },
+  {
+    type: "expression",
+    label: "Format Time",
+    opcode: "time.format",
+    category: "time",
+    slots: [
+      { name: "Time", type: "block" },
+      { name: "Format", type: "string" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Offset Time",
+    opcode: "time.offset",
+    category: "time",
+    slots: [
+      { name: "Time", type: "block" },
+      { name: "Amount", type: "number" },
+      { name: "Unit", type: "string" },
+    ],
+  },
+
+  // World
+  {
+    type: "expression",
+    label: "All Entities",
+    opcode: "world.entities",
+    category: "world",
+    slots: [],
+  },
+  {
+    type: "expression",
+    label: "Contents",
+    opcode: "entity.contents",
+    category: "world",
+    slots: [{ name: "Target", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "Descendants",
+    opcode: "entity.descendants",
+    category: "world",
+    slots: [{ name: "Target", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "Ancestors",
+    opcode: "entity.ancestors",
+    category: "world",
+    slots: [{ name: "Target", type: "block" }],
+  },
+
+  // List
+  {
+    type: "expression",
+    label: "List Len",
+    opcode: "list.len",
+    category: "list",
+    slots: [{ name: "List", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "List Get",
+    opcode: "list.get",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Index", type: "number" },
+    ],
+  },
+  {
+    type: "statement",
+    label: "List Set",
+    opcode: "list.set",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Index", type: "number" },
+      { name: "Value", type: "block" },
+    ],
+  },
+  {
+    type: "statement",
+    label: "List Push",
+    opcode: "list.push",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Value", type: "block" },
+    ],
+  },
+  {
+    type: "statement",
+    label: "List Pop",
+    opcode: "list.pop",
+    category: "list",
+    slots: [{ name: "List", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "List Map",
+    opcode: "list.map",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Func", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "List Filter",
+    opcode: "list.filter",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Func", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "List Reduce",
+    opcode: "list.reduce",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Func", type: "block" },
+      { name: "Init", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "List FlatMap",
+    opcode: "list.flatMap",
+    category: "list",
+    slots: [
+      { name: "List", type: "block" },
+      { name: "Func", type: "block" },
+    ],
+  },
+
+  // Object
+  {
+    type: "expression",
+    label: "Obj Keys",
+    opcode: "obj.keys",
+    category: "object",
+    slots: [{ name: "Obj", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "Obj Values",
+    opcode: "obj.values",
+    category: "object",
+    slots: [{ name: "Obj", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "Obj Entries",
+    opcode: "obj.entries",
+    category: "object",
+    slots: [{ name: "Obj", type: "block" }],
+  },
+  {
+    type: "expression",
+    label: "Obj Get",
+    opcode: "obj.get",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Key", type: "string" },
+    ],
+  },
+  {
+    type: "statement",
+    label: "Obj Set",
+    opcode: "obj.set",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Key", type: "string" },
+      { name: "Value", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Obj Has",
+    opcode: "obj.has",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Key", type: "string" },
+    ],
+  },
+  {
+    type: "statement",
+    label: "Obj Del",
+    opcode: "obj.del",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Key", type: "string" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Obj Merge",
+    opcode: "obj.merge",
+    category: "object",
+    slots: [
+      { name: "A", type: "block" },
+      { name: "B", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Obj Map",
+    opcode: "obj.map",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Func", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Obj Filter",
+    opcode: "obj.filter",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Func", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Obj Reduce",
+    opcode: "obj.reduce",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Func", type: "block" },
+      { name: "Init", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Obj FlatMap",
+    opcode: "obj.flatMap",
+    category: "object",
+    slots: [
+      { name: "Obj", type: "block" },
+      { name: "Func", type: "block" },
+    ],
+  },
+
+  // Functions
+  {
+    type: "expression",
+    label: "Lambda",
+    opcode: "lambda",
+    category: "func",
+    slots: [
+      { name: "Args", type: "block" }, // Should be list of strings
+      { name: "Body", type: "block" },
+    ],
+  },
+  {
+    type: "expression",
+    label: "Apply",
+    opcode: "apply",
+    category: "func",
+    slots: [
+      { name: "Func", type: "block" },
+      { name: "Args...", type: "block" }, // This is variadic, might need special handling in UI
+    ],
   },
 ];
